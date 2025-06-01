3be2533cddbe0112a42c2332cbf92451d13da84d
Areeb Khan <areebsam2@gmail.com>
2025-06-01 11:23:51 -0700

    Create main.py

---
+++ Age-( If else use )/main.py
@@ -0,0 +1,16 @@
+#Polishing if statements use in cases of greater/smaller and equal to vaues
+age = int(input("Enter your age: "))
+   
+if age ==100:
+ print("You are a Century old!")
+
+elif age >100:
+    print("You are older than a Century!")
+    
+elif age >= 18:
+    print("You are an Adult!")
+    
+else:
+ print("You are a Child :(")
+
+     
