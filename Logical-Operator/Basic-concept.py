341314a1447574d5ccdce73f39b5e4ed03e9e579
Areeb Khan <areebsam2@gmail.com>
2025-06-02 10:52:14 -0700

    Create Basic-concept.py

---
+++ Logical-Operator/Basic-concept.py
@@ -0,0 +1,8 @@
+#Logical Operators !
+temp = float(input("Enter Current Temperature (Celsius): "))
+if temp <= 10: 
+    print("Don't go outside, Its too cold!")
+elif temp <= 39:
+ print("Its Normal, You can go outside!")
+elif temp > 40:
+    print("The weather is gonna cook u up outside!")
