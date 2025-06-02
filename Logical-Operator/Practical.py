f9b6e5b4a185fd9629664fd9c3662745776dfba9
Areeb Khan <areebsam2@gmail.com>
2025-06-02 10:57:15 -0700

    Create Practical.py

---
+++ Logical-Operator/Practical.py
@@ -0,0 +1,11 @@
+#Logical Operators !
+temp = float(input("Enter Current Temperature (Celsius): "))
+if temp <= 20: 
+    #Any code to turn on a Heater:)
+    print("------Turn on heater!-------")
+elif temp <= 25:
+ print("------Its Normal!-------")
+  # Will Do nothing Cuz U can Survive At this Temperature
+elif temp > 26:
+    #Any code/Automaion To turn on a Ac :)
+    print("--------Turn on the AC!!----------")
