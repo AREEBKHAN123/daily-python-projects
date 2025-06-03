92ee4eb99d81bdc0d0940f1938bd7eaa2de41898
Areeb Khan <areebsam2@gmail.com>
2025-06-03 23:15:25 +0500

    Create Concept.py

---
+++ While Loops/Concept.py
@@ -0,0 +1,11 @@
+#🔁 What Is a while Loop?
+#A while loop in Python is used to repeat a block of code as long as a certain condition is True.
+
+#Think of it like saying:
+#🗣️ "Keep doing this until the condition is no longer true."
+count = 1 
+while count<9999 :
+ print(count)
+ count += 1
+ 
+ #It stops after count becomes 9999 addding 1 until count becomes greater than 9998
